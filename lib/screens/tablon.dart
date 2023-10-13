898bd027cc76c92386fb8c3508bd81a9aaea4c97
lanza10 <jlanza100@gmail.com>
2023-10-13 19:43:44 +0200

    barra navegacion en tablon

--- lib/screens/tablon.dart
+++ lib/screens/tablon.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:nest_fronted/widgets/barra_titulo.dart';
 
 const tituloScreen = 'MI TABLÓN PERSONAL';
+int selectedIndex = 0;
 
 class TablonScreen extends StatelessWidget {
   const TablonScreen({super.key});
@@ -9,15 +10,67 @@ class TablonScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        body: Column(
-      children: [
-        //Barra de título
-        BarraTitulo(titulo: tituloScreen),
+      body: Column(
+        children: [
+          //Barra de título
+          BarraTitulo(titulo: tituloScreen),
 
-        //Tablón
-        Tablon()
-      ],
-    ));
+          //Tablón
+          Tablon()
+        ],
+      ),
+      bottomNavigationBar: BarraNavegacion(),
+    );
+  }
+}
+
+class BarraNavegacion extends StatefulWidget {
+  const BarraNavegacion({
+    super.key,
+  });
+
+  @override
+  State<BarraNavegacion> createState() => _BarraNavegacionState();
+}
+
+class _BarraNavegacionState extends State<BarraNavegacion> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        currentIndex: selectedIndex,
+        onTap: (value) {
+          setState(() {
+            selectedIndex = value;
+          });
+        },
+        elevation: 0,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            activeIcon: Icon(Icons.home_filled),
+            label: 'Home',
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            activeIcon: Icon(Icons.saved_search),
+            label: 'Search',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            activeIcon: Icon(Icons.group_add),
+            label: 'Friends',
+            backgroundColor: Colors.purpleAccent,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            activeIcon: Icon(Icons.settings_applications),
+            label: 'Settings',
+            backgroundColor: Colors.teal,
+          ),
+        ]);
   }
 }
 
@@ -32,7 +85,7 @@ class Tablon extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
       color: Colors.brown,
-      height: 700,
+      height: 600,
       width: 700,
       child: const Column(
         children: [
