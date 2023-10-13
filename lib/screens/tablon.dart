bc666ef244cdd0c9f02bc10e6d1c5c99dd399135
lanza10 <jlanza100@gmail.com>
2023-10-13 10:25:30 +0200

    Estructura + inicio tablón

---
+++ lib/screens/tablon.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TablonScreen extends StatelessWidget {
+  const TablonScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('HOLA MUNDO'),
+      ),
+    );
+  }
+}
