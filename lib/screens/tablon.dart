9b24b40c6af7b1c80f4b9f41b865b0eb78682da6
lanza10 <jlanza100@gmail.com>
2023-10-13 18:34:51 +0200

    tablon prueba + barraTitulo widget

--- lib/screens/tablon.dart
+++ lib/screens/tablon.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nest_fronted/widgets/barra_titulo.dart';
+
+const tituloScreen = 'MI TABLÓN PERSONAL';
 
 class TablonScreen extends StatelessWidget {
   const TablonScreen({super.key});
@@ -6,8 +9,39 @@ class TablonScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('HOLA MUNDO'),
+        body: Column(
+      children: [
+        //Barra de título
+        BarraTitulo(titulo: tituloScreen),
+
+        //Tablón
+        Tablon()
+      ],
+    ));
+  }
+}
+
+//Se ha de cambiar por un stack para la foto de fondo
+class Tablon extends StatelessWidget {
+  const Tablon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      color: Colors.brown,
+      height: 700,
+      width: 700,
+      child: const Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text('prueba')],
+          )
+        ],
       ),
     );
   }
