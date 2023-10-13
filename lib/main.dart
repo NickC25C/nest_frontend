1d7677b8e6df868949f6c920d4c3ae1da760c6c1
elAnton777 <alejandro.antoncabezas@gmail.com>
2023-10-13 23:24:28 +0200

    interfaz top publicar notas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nest_fronted/screens/tablon.dart';
+import 'package:nest_fronted/screens/pub_nota.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,7 +12,37 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       initialRoute: 'tablon',
-      routes: {'tablon': (_) => const TablonScreen()},
+      routes: {'tablon': (_) => const PubNotaScreen()},
     );
   }
 }
+//iconos de flechas para retroceder o pasar a siguiente
+class flechas extends StatelessWidget {
+  const flechas({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 25.0),
+          child: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {},
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 25.0),
+          child: IconButton(
+            icon: Icon(Icons.arrow_forward),
+            onPressed: () {},
+          ),
+        ),
+      ],
+    );
+  }
+}
+
---
+++ lib/screens/pub_nota.dart
@@ -0,0 +1,234 @@
+import 'package:flutter/material.dart';
+import 'package:nest_fronted/widgets/barra_titulo.dart';
+
+const tituloScreen = 'PUBLICAR NOTA';
+int selectedIndex = 0;
+
+
+class PubNotaScreen extends StatelessWidget {
+  const PubNotaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          //Barra de título
+          BarraTitulo(titulo: tituloScreen),
+
+          Flechas(),
+
+          Titulin(),
+
+          Nota(),
+
+          EnviarA(),
+
+        ],
+      ),
+      bottomNavigationBar: BarraNavegacion(),
+    );
+  }
+}
+
+class BarraNavegacion extends StatefulWidget {
+  const BarraNavegacion({
+    super.key,
+  });
+
+  @override
+  State<BarraNavegacion> createState() => _BarraNavegacionState();
+}
+
+class _BarraNavegacionState extends State<BarraNavegacion> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        currentIndex: selectedIndex,
+        onTap: (value) {
+          setState(() {
+            selectedIndex = value;
+          });
+        },
+        elevation: 0,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            activeIcon: Icon(Icons.home_filled),
+            label: 'Home',
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            activeIcon: Icon(Icons.saved_search),
+            label: 'Search',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            activeIcon: Icon(Icons.group_add),
+            label: 'Friends',
+            backgroundColor: Colors.purpleAccent,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            activeIcon: Icon(Icons.settings_applications),
+            label: 'Settings',
+            backgroundColor: Colors.teal,
+          ),
+        ]);
+  }
+}
+
+//iconos de flechas para retroceder o pasar a siguiente
+class Flechas extends StatelessWidget {
+  const Flechas({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.arrow_forward),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
+
+//titulo de la nota
+class Titulin extends StatelessWidget {
+  const Titulin({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Título:',
+          style: TextStyle(
+            fontSize: 17.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(width: 8.0),
+        Container(
+          width: 200.0,
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: 'Nick putero',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+//contenido de la nota
+class Nota extends StatelessWidget {
+  const Nota({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 40.5),
+        Text(
+          'Mensaje:',
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 25.5),
+        Container(
+          width: 350,
+          padding: EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(color: Colors.black),
+          ),
+          child: TextField(
+            maxLines: 15,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Lanza porreta',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+//Seleccionar a los usuarios/grupos a los q enviar
+class EnviarA extends StatelessWidget {
+  const EnviarA({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 25.5),
+        Text(
+          'Enviar a:',
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 25.5),
+        Container(
+          height: 150.0,
+          width: 350,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: ListView(
+            padding: EdgeInsets.all(0.0),
+            children: [
+              ListTile(
+                title: Text(
+                'Antón',
+                style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+              ListTile(
+                title: Text(
+                'Lanza',
+                style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  'Colau',
+                style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
