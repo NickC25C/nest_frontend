b2d9aec1a726e434112102fbaacd868f9f1dd307
NickC25C <niccm25@gmail.com>
2023-10-15 19:14:34 +0200

    terminada interfaz de publicar imagen

--- lib/screens/pub_imagen.dart
+++ lib/screens/pub_imagen.dart
@@ -1,25 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:nest_fronted/widgets/barra_publicar.dart';
 
-const tituloScreen = 'PUBLICAR NOTA';
+const tituloScreen = 'PUBLICAR IMAGEN';
 
 class PubImagenScreen extends StatelessWidget {
   const PubImagenScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           BarraPublicar(titulo: tituloScreen),
-          SizedBox(height: 20.0,),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               BotonIzquierdo(),
               BotonDerecho(),
             ],
+          ),
+          AnyadirImagen(imagen: 'assets/images/anyadir_imagen.png'),
+          Titulo(),
+          Column(
+            children: [
+              DropdownSample(),
+              Listado(),
+            ],
           )
+
         ],
       )
     );
@@ -94,6 +103,114 @@ class BotonDerecho extends StatelessWidget {
               )
           ),
         ),
+      ),
+    );
+  }
+}
+
+class AnyadirImagen extends StatelessWidget {
+  final String imagen;
+  const AnyadirImagen({
+    super.key,
+    required this.imagen,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Image(
+      image: AssetImage(imagen),
+      width: 150.0,
+      height: 150.0,
+      fit: BoxFit.fill,
+    );
+  }
+
+}
+
+class Titulo extends StatelessWidget {
+  const Titulo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: Container(
+            width: double.infinity,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText : 'Titulo',
+                border: UnderlineInputBorder(),
+                hintText: 'La Rata Ladrona',
+              ),
+            ),
+      ),
+    );
+  }
+}
+
+class DropdownSample extends StatefulWidget {
+  @override
+  _Enviar createState() => _Enviar();
+}
+
+class _Enviar extends State<DropdownSample> {
+  String? selectedValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Enviar A:'),
+            Container(
+              width: double.infinity,
+              child: DropdownButton<String>(
+              items: <String>['Opción 1', 'Opción 2'].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+
+              value: selectedValue,
+              hint: Text('Selecciona una opción'),
+              onChanged: (String? newValue){
+                setState(() {
+                  selectedValue = newValue;
+                });
+              }),
+            ),
+          ],
+        ),
+      );
+  }
+}
+
+class Listado extends StatelessWidget {
+  const Listado({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 130,
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.blue, width: 2),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ListView(
+        children: [
+          ListTile(title: Text('Ítem 1'),),
+          ListTile(title: Text('Ítem 2')),
+          ListTile(title: Text('Ítem 3')),
+        ],
       ),
     );
   }
