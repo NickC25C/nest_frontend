95479daa8220bb193aae25d58fb39d91e67156e7
lanza10 <jlanza100@gmail.com>
2023-10-19 11:36:53 +0200

    emepezando visualizar foto

--- lib/widgets/foto.dart
+++ lib/widgets/foto.dart
@@ -1,33 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
 
-class Foto extends StatelessWidget {
+class Foto extends StatefulWidget {
   final String url;
   const Foto({
     super.key,
     required this.url,
   });
+  @override
+  State<Foto> createState() => _FotoState();
+}
+
+class _FotoState extends State<Foto> {
+  late final String fotoUrl;
+
+  void initState() {
+    super.initState();
+    fotoUrl = widget.url;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      height: 250,
-      width: 200,
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/images/foto.png'),
-              alignment: Alignment.center,
-              fit: BoxFit.contain)),
-      child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black),
-          ),
-          height: 150,
-          width: 150,
-          child: Image(
-            image: AssetImage(url),
-            fit: BoxFit.cover,
-          )),
-    );
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black),
+        ),
+        height: 300,
+        width: 300,
+        child: Image(
+          image: AssetImage(fotoUrl),
+          fit: BoxFit.cover,
+        ));
   }
 }
