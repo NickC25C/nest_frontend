d4feac2ce93c4a7ae60d5d171552649feee3e8ca
d34dHusky <d34dHusky@users.noreply.github.com>
2023-10-24 14:12:31 +0200

    Publication API connection

---
+++ lib/models/publication.dart
@@ -0,0 +1,34 @@
+import 'package:nest_fronted/models/user.dart';
+
+enum PublicationType {
+  note,
+  picture,
+  song
+}
+
+class Publication {
+  final int id;
+  final User owner;
+  final DateTime date;
+  final PublicationType publiType;
+
+  Publication({required this.id, required this.owner, required this.date, required this.publiType});
+
+  factory Publication.fromJson(Map<String, dynamic> json) {
+    return Publication(
+        id: json['id'],
+        owner: json['owner'],
+        date: json['date'],
+        publiType: json['publiType'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'owner': owner,
+      'date': date,
+      'publiType': publiType
+    };
+  }
+}
